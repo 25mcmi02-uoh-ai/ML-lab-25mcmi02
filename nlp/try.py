210740486a92aff80b44b3f521bd5fcc17151f4f
Mohammed Ali <25mcmi02@uohyd.ac.in>
2026-02-28 06:09:14 +0000

    hello

--- nlp/try.py
+++ nlp/try.py
@@ -1,11 +1,14 @@
+import re
+
 # Byte Pair Encoding (BPE)
 
 def byte_pair_encoding(corpus, limit=100):
     # get initial vocabulary
+    # replace space with '_' then add space after every character in the corpus
+
+    corpus = corpus.replace(" ", "_")
     vocabulary = sorted(list(set(list(corpus))))
     
-    # replace space with '_' then add space after every character in the corpus
-    corpus = corpus.replace(" ", "_")
     corpus = list(corpus)
         
     # repeat k times
@@ -13,6 +16,8 @@ def byte_pair_encoding(corpus, limit=100):
         # find the most frequent pair
         frequent_pair = get_most_frequent_pair(corpus, vocabulary)
         
+        if len(frequent_pair) == 0:
+            break
         # add to the vocabulary
         vocabulary.append(frequent_pair[0]+frequent_pair[1])
         
@@ -22,28 +27,55 @@ def byte_pair_encoding(corpus, limit=100):
     return vocabulary
 
 def get_most_frequent_pair(corpus, vocabulary):
-    
+    most_frequent = []
+    most_frequency = 0
 
+    for v1 in vocabulary:
+        for v2 in vocabulary:
+            if v1[-1] == '_':
+                break
+            current = find_count_pair(corpus, v1, v2)
+            if current > most_frequency:
+                most_frequent = [v1, v2]
+                most_frequency = current
 
-    return ['e', 'r']
+    return most_frequent
+
+def find_count_pair(corpus, v1, v2):
+    counter = 0
+    s = 1
+    l = len(corpus)
+
+    while True:
+        try:
+            found = corpus.index(v1, s)
+        except ValueError:
+            break
+        if found+1 != l and corpus[found+1] == v2:
+            counter += 1
+        s = found+1
+    return counter
 
 def replace_pair(corpus, frequent_pair):
+    s = 0
     length = len(corpus)
     v1 = frequent_pair[0]
     v2 = frequent_pair[1]
-    
+
     while True:
         try:
-            found = corpus.index(v1)
+            found = corpus.index(v1, s)
         except ValueError:
             break
-
-        if corpus[found+1] == v2:
-            # drop both elements from list and add the combined in that location
-            corpus.insert(found, v1+v2)
-            corpus.pop(found+1)
-            corpus.pop(found+2)
+        if found+1 < length:
+            if corpus[found+1] == v2:
+                # drop both elements from list and add the combined in that location
+                corpus.insert(found, v1+v2)
+                corpus.pop(found+1)
+                corpus.pop(found+1)
+        else:
+            s = found+1
 
 
 corpus = "low low low low lower lower lower new new new new new newer newer newer"
-print(byte_pair_encoding(corpus, limit=3))
+print(byte_pair_encoding(corpus, limit=10))
